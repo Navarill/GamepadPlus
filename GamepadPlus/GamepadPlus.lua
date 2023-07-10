7835db1808bbbaf72b0b67f679f1db592542b88e
Navarill <6269482+Navarill@users.noreply.github.com>
2023-07-10 12:10:32 -0500

    API & Version Bump

--- GamepadPlus/GamepadPlus.lua
+++ GamepadPlus/GamepadPlus.lua
@@ -18,7 +18,7 @@ local GPP = GamepadPlus
 GPP.name = "GamepadPlus"
 GPP.title = "Gamepad Plus"
 GPP.author = "Navarill"
-GPP.version = "2.0.0"
+GPP.version = "2.1.0"
 GPP.settings = {}
 
 -- FormattedNumber
